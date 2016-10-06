f74f0a852cad31e0edcfbc63b983f2c52948cbe5
David McClure <davidwilliammcclure@gmail.com>
2016-10-06 16:37:09 -0700

    Install Flask-Admin.

--- poetics/app.py
+++ poetics/app.py
@@ -1,23 +1,32 @@
 
 
 from flask import Flask
+from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 
-from .extensions import db, migrate
-
-# Register models.
-from .models import *
+from .extensions import db, migrate, admin
+from .models import Text
 
 
 app = Flask(__name__)
 
+app.secret_key = 'dev'
+
 # TODO: dev, prod.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/poetics.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 # TODO: Break out app factory.
+
+admin = Admin(template_mode='bootstrap3')
+admin.add_view(ModelView(Text, db.session))
+
 db.init_app(app)
 migrate.init_app(app, db)
+admin.init_app(app)
 
 
 @app.route('/')
--- poetics/extensions.py
+++ poetics/extensions.py
@@ -1,9 +1,12 @@
 
 
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 
 
 db = SQLAlchemy()
 
 migrate = Migrate()
+
+admin = Admin()
