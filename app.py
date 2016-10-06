5e746bd053443c4ae583c01ad0193dffc85b11fd
David McClure <davidwilliammcclure@gmail.com>
2016-10-06 14:32:35 -0700

    Add migrations.

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Text(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String, nullable=False)
 
 
 @app.route('/')
---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='field-poetics',
+    version='0.1.0',
+    description='How do people read?',
+    url='https://github.com/davidmcclure/field-poetics',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+
+)
