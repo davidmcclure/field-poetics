a4f2a3814d145663fe24991afc1492af2db79dfa
David McClure <davidwilliammcclure@gmail.com>
2016-10-11 18:05:44 -0700

    Don't allow changes to the text markup after creation.

--- studies/admin.py
+++ studies/admin.py
@@ -5,4 +5,19 @@ from django.contrib import admin
 from studies.models import Text
 
 
-admin.site.register(Text)
+class TextAdmin(admin.ModelAdmin):
+
+    def get_readonly_fields(self, request, obj):
+
+        """
+        Don't allow edits to the markup after creation.
+        """
+
+        if obj:
+            return self.readonly_fields + ('markup',)
+
+        else:
+            return self.readonly_fields
+
+
+admin.site.register(Text, TextAdmin)
