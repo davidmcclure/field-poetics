0c4587e0bcf7536198b344186a982414c2ea6b5a
David McClure <davidwilliammcclure@gmail.com>
2016-10-11 17:32:35 -0700

    Add a Text model.

--- studies/models.py
+++ studies/models.py
@@ -5,4 +5,11 @@ from django.db import models
 
 class Text(models.Model):
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(
+        max_length=200,
+    )
+
+    markup = models.TextField()
+
+    def __str__(self):
+        return self.title
