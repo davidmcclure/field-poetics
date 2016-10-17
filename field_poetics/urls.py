7d43d6e09c6237f438066bf8bbb606c4c621f900
David McClure <davidwilliammcclure@gmail.com>
2016-10-17 16:32:44 -0700

    Wiring up basic views.

--- field_poetics/urls.py
+++ field_poetics/urls.py
@@ -1,11 +1,12 @@
 
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^studies/', include('studies.urls')),
 ]
 
 
--- studies/views.py
+++ studies/views.py
@@ -2,4 +2,17 @@
 
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Study
+
+
+def text(request, slug):
+
+    """
+    Mark the text.
+    """
+
+    study = Study.objects.get(slug=slug)
+
+    cond, text = study.randomized_text()
+
+    return render(request, 'studies/text.html', dict(text=text))
